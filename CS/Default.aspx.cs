f71747bc3ee87398d88995e1f21ef296ceb86f62
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,9 @@
+﻿public partial class _Default : System.Web.UI.Page {
+    protected void UploadControl_FileUploadComplete(object sender, DevExpress.Web.FileUploadCompleteEventArgs e) {
+        if (e.UploadedFile.IsValid) {
+            string fileName = e.UploadedFile.FileName;
+            e.UploadedFile.SaveAs(MapPath(RichEdit.WorkDirectory) + "\\" + fileName, true);
+            e.CallbackData = fileName;
+        }
+    }
+}
---
+++ VB/Default.aspx.vb
@@ -0,0 +1,11 @@
+﻿Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub UploadControl_FileUploadComplete(ByVal sender As Object, ByVal e As DevExpress.Web.FileUploadCompleteEventArgs)
+        If e.UploadedFile.IsValid Then
+            Dim fileName As String = e.UploadedFile.FileName
+            e.UploadedFile.SaveAs(MapPath(RichEdit.WorkDirectory) & "\" & fileName, True)
+            e.CallbackData = fileName
+        End If
+    End Sub
+End Class
